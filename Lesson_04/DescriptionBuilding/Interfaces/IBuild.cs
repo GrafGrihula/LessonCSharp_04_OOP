a6d862e77b28ad976759cf6de1b7d8901f3a9359
GrafGrihula <grafgrihula@yandex.ru>
2021-11-21 18:45:46 +0300

    Update IBuild.cs

--- Lesson_04/DescriptionBuilding/Interfaces/IBuild.cs
+++ Lesson_04/DescriptionBuilding/Interfaces/IBuild.cs
@@ -3,10 +3,13 @@
     public interface IBuild
     {
         string Operation();
+<<<<<<< Updated upstream
         int Id { get; set; }                 // Уникальный номер здания
         double Height { get; set; }          // Высота здания
         int Floors { get; set; }             // Этажность здания
         int NumberOfApartments { get; set; } // Количество квартир
         int NumberOfEntrances { get; set; }  // Количество подъездов
+=======
+>>>>>>> Stashed changes
     }
 }
